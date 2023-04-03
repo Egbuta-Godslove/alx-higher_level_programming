f3d3e2fc05979c42318609449d3aec7f09e31bac
Egbuta-Godslove <godsloveegbuta6@gmail.com>
2023-04-03 09:35:10 -0700

    correction 2-rectangle.py

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -24,8 +24,8 @@ class Rectangle:
 	 """setter for private instance attribute width"""
         if type(value, int):
             raise TypeError("width must be an integer")
-        elif value < 0:
-            raise ValueError("width must be >= 0")
+        if value < 0:
+	    raise ValueError("width must be >= 0")
     
     @property
     def height(self):
@@ -36,7 +36,7 @@ class Rectangle:
 	"""getter for private instance attribute height"""    
         if type(value, int):
             raise TypeError("height must be an integer")
-        elif value < 0:
+        if value < 0:
             raise ValueError("height must be >= 0")
     
     def area(self):  
