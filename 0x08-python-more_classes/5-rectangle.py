19946931e0b6c28088fbdbbe3c1ff908efc9bc89
Egbuta-Godslove <godsloveegbuta6@gmail.com>
2023-04-03 13:06:44 -0700

    5. Detect instance deletion

--- 0x08-python-more_classes/5-rectangle.py
+++ 0x08-python-more_classes/5-rectangle.py
@@ -1,26 +1,22 @@
 #!/usr/bin/python3
-"""
-Defines a class Rectangle
-"""
-"""ALX task on OOP"""
+"""Module with Rectangle class"""
+
 
 class Rectangle:
-    """Representation of a rectangle"""
+    """Rectangle class"""
     def __init__(self, width=0, height=0):
-        """starting methodof the rectangle by
-        setting the objet with width and height
-        """
+        """init method """
         self.width = width
-        self.height = height
+        self.height = heigt
 
     @property
     def width(self):
-        """Getter method for  width"""
+        """width getter"""
         return self.__width
 
     @width.setter
     def width(self, value):
-        """Setter method for width"""
+        """width setter"""
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         if value < 0:
@@ -29,12 +25,12 @@ class Rectangle:
 
     @property
     def height(self):
-        """Getter method for height"""
+        """height height"""
         return self.__height
 
     @height.setter
     def height(self, value):
-        """Setter method for height"""
+        """height setter"""
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
@@ -42,19 +38,27 @@ class Rectangle:
         self.__height = value
 
     def area(self):
-        """returns the area of the rectangle"""
-        return 2 * (self.width + self.height)
+        """area method"""
+        return self.__width * self.__height
 
     def perimeter(self):
-        """returns the perimeter of the rectangle"""
+        """perimeter method"""
         if self.width == 0 or self.height == 0:
             return 0
-        return (self.width * 2) + (self.height * 2)
+        return 2 * (self.width + self.height)
 
     def __str__(self):
-        """returns printable string representation of the rectangle"""
+        """str method"""
         string = ""
         if self.__width != 0 and self.__height != 0:
             string += "\n".join("#" * self.__width
                                 for j in range(self.__height))
         return string
+
+    def __repr__(self):
+        """returns a string representation of the rectangle for reproduction"""
+        return "Rectangle({:d}, {:d})".format(self.__width, self.__height)
+
+    def __del__(self):
+	"""del method"""
+	print("Bye rectangle...")	
