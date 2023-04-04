68091361a8856ace0df055c770d9151eaf86384b
Egbuta-Godslove <godsloveegbuta6@gmail.com>
2023-04-04 11:12:45 -0700

    update

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,4 +1,15 @@
 #!/usr/bin/python3
-def magic_string(List_of_str=[]):
-    List_of_str += ["BestSchool"]
-    return (", ".join(List_of_str))
+
+def magic_string(n):
+    
+    Args:
+        n (int): The number of iterations.
+
+    Returns:
+        str: A string containing "BestSchool" reapeat n times for each iteration.
+    
+    result = " "    
+    for i in range(1, n+1):
+        result += "BestSchool" *i
+    return result
+
