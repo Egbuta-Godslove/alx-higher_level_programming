63699203ab1d10fe8c233dfc7a5332defd64674a
Egbuta-Godslove <godsloveegbuta6@gmail.com>
2023-04-04 11:26:18 -0700

    added solution

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -4,6 +4,8 @@ class LockedClass:
     __slots__ = ['first_name']
 
     def __setattr__(self, key, value):
-        if key != 'first_name' or not hasattr(self, 'first_name'):
+        if key == 'first_name' and hasattr(self, 'first_name'):
+            super().__setattr__(key, value)
+        else:
             raise AttributeError(f"Cannot set attribute '{key}' on LockedClass")
-        super().__setattr__(key, value)
+
