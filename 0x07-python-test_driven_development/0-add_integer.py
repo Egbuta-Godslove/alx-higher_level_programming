a3be0ad91c11cc9d026c4a7a6ab7d199df9f06a3
Egbuta-Godslove <godsloveegbuta6@gmail.com>
2023-04-04 14:32:19 -0700

    task 0

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,8 +1,20 @@
 #!/usr/bin/python3
+""" 0-add_integer Module """
+
 def add_integer(a, b=98):
-  if type(a) not in [int, float] or type(b) not in [int, float]:
-    raise TypeError
-  else:
-    a = int(a)
-    b = int(b)
-    return a + b
+    """
+    Adds two integers
+    Args:
+        a: first integer
+        b: second integer
+    Returns:
+        addition of two integers
+    """
+    if not isinstance(a, (int, float)):
+        raise TypeError("a must be an integer")
+    elif not isinstance(b, (int, float)):
+        raise TypeError("b must be an integer")
+    else:
+        new_a, new_b = int(a), int(b)
+        return new_a + new_b
+
