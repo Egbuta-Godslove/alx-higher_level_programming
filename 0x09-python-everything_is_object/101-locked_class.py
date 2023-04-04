3bc5700a8058244e00dbbd268e65ea5523f3ac3c
Egbuta-Godslove <godsloveegbuta6@gmail.com>
2023-04-04 13:39:16 -0700

    #!/usr/bin/python3

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -2,10 +2,3 @@
 
 class LockedClass:
     __slots__ = ('__first_name')
-
-    def __init__(self, first_name):
-        self.__first_name = first_name
-
-    @property
-    def first_name(self):
-        return self.__first_name
