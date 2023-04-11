7fbd142bd26635804fb6e66fe2ea02f2ac269b71
Egbuta-Godslove <godsloveegbuta6@gmail.com>
2023-04-11 12:43:56 -0700

    pycodestyle formate

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -3,16 +3,21 @@
 This module contains the function is_instance_of_class.
 """
 
+
 def is_instance_of_class(instance, class_type):
+
     """
-    This function checks if the given instance is an instance of the specified class.
-    
+    This function checks if the given instance is an
+    instance of the specified class.
+
     Args:
     instance: The instance to check
     class_type: The class to check against
-    
+:wq
+
     Returns:
-    True if the instance is an instance of the specified class, False otherwise.
+    True if the instance is an instance of the specified class,
+    False otherwise.
     """
     if type(instance) is not class_type:
         return False
