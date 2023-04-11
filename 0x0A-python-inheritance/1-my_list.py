a66c12b90869ae3cac560dec6cc542da4b3c8fb6
Egbuta-Godslove <godsloveegbuta6@gmail.com>
2023-04-11 12:15:28 -0700

    task 1 with no txt

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,15 @@
+class SortedList(list):
+    """
+    A custom list class that inherits from the built-in list and adds
+    a new method `print_sorted`
+    to print the sorted list.
+    """
+
+    def __init__(self, *args, **kwargs):
+        """Initialize the list."""
+        super().__init__(*args, **kwargs)
+
+    def print_sorted(self):
+        """Print the sorted list."""
+        sorted_list = sorted(self)
+        print(sorted_list)
