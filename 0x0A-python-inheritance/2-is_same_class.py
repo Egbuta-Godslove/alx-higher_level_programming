460c1fadbf9877f9dfa23bf4b755aa51e45e7113
Egbuta-Godslove <godsloveegbuta6@gmail.com>
2023-04-11 12:36:17 -0700

    task 2

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+This module contains the function is_instance_of_class.
+"""
+
+def is_instance_of_class(instance, class_type):
+    """
+    This function checks if the given instance is an instance of the specified class.
+    
+    Args:
+    instance: The instance to check
+    class_type: The class to check against
+    
+    Returns:
+    True if the instance is an instance of the specified class, False otherwise.
+    """
+    if type(instance) is not class_type:
+        return False
+    else:
+        return True
