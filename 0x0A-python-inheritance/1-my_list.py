86c845f50f6b50eb1c9ad7fb95bfd381a4b65e17
Egbuta-Godslove <godsloveegbuta6@gmail.com>
2023-04-11 12:32:44 -0700

    task 1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,6 +1,18 @@
 #!/usr/bin/python3
 
 class MyList(list):
-    def print_sorted(self):
+    """
+    A subclass of list that represents a custom list
+    """
+    def __init__(self):
+        """
+        Initializes an instance of MyList
+        """
+        super().__init__()
+
+    def print_sorted_list(self):
+        """
+        Prints the sorted list
+        """
         sorted_list = sorted(self)
         print(sorted_list)
